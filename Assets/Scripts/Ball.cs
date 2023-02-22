4cafc97acd690f179b56ffaa9fc6c43e68f50ae2
Julin7 <121979782+Julin7@users.noreply.github.com>
2023-02-22 13:18:37 -0800

    Video 3 Part 1

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    
+    public float force = 100f;
     private Vector2 startPosition;
     private Vector2 endPosition;
+    private Rigidbody2D physics;
+    
+
+    void Awake(){
+        physics = GetComponent<Rigidbody2D>();
+    }
 
     void Start()
     {
-        
+        physics.isKinematic = true;
     }
 
    
@@ -18,14 +24,21 @@ public class Ball : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0)){
            
-            startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            startPosition = getMousePosition();
             Debug.Log(startPosition);
         }
          if(Input.GetMouseButtonUp(0)){
-           
-            endPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-             Debug.Log(endPosition);
+                endPosition = getMousePosition();
+
+             Vector2 power = startPosition - endPosition;
+            physics.isKinematic = false;
+
+             physics.AddForce(power * force, ForceMode2D.Force);
         }
         
+    }
+
+    private Vector2 getMousePosition(){
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 }
