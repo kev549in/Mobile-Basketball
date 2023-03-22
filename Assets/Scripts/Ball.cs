d5f2ddfd91b107f029f609c0bce7f58508c0702a
erliang69 <121979639+erliang69@users.noreply.github.com>
2023-03-22 12:58:11 -0700

    Video 10

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -69,6 +69,7 @@ public class Ball : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if(!collision.gameObject.tag.Equals("ground")) return;
         physics.isKinematic = true;
         transform.position = defaultBallPosition;
         physics.velocity = Vector2.zero;
